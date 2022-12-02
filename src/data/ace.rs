f9a1f31b9829ad363c3baa2d985af00760e99e44
Loky HARK <loky.hark@pm.me>
2022-12-02 11:23:04 +0100

    Full ACE format support

--- src/data/ace.rs
+++ src/data/ace.rs
@@ -3,6 +3,7 @@
 use std::error::Error as StdError;
 use std::fmt::Display;
 use std::io::{Error as IOError, Read};
+use std::str::Lines;
 
 /// ACE Table.
 #[derive(Clone, Debug, PartialEq)]
@@ -79,14 +80,19 @@ fn parse_table_version1(ace: String) -> Result<Table, AceError> {
     let Ok(temperature) = line[22..34].trim().parse() else {
         return Err(AceError::Format)
     };
+    iter.next();
+    let izaw = parse_izaw_array(&mut iter)?;
+    let nxs = parse_nxs_array(&mut iter)?;
+    let jxs = parse_jxs_array(&mut iter)?;
+    let xss = parse_xss_array(&mut iter, nxs[0])?;
     Ok(Table {
         id,
         atomic_weight_ratio,
         temperature,
-        izaw: Vec::new(),
-        nxs: Vec::new(),
-        jxs: Vec::new(),
-        xss: Vec::new(),
+        izaw,
+        nxs,
+        jxs,
+        xss,
     })
 }
 
@@ -105,15 +111,99 @@ fn parse_table_version2(ace: String) -> Result<Table, AceError> {
     let Ok(temperature) = line[13..25].trim().parse() else {
         return Err(AceError::Format)
     };
+    let Ok(comment) = line[37..].trim().parse() else {
+        return Err(AceError::Format)
+    };
+    for _ in 0..comment {
+        iter.next();
+    }
+    let izaw = parse_izaw_array(&mut iter)?;
+    let nxs = parse_nxs_array(&mut iter)?;
+    let jxs = parse_jxs_array(&mut iter)?;
+    let xss = parse_xss_array(&mut iter, nxs[0])?;
     Ok(Table {
         id,
         atomic_weight_ratio,
         temperature,
-        izaw: Vec::new(),
-        nxs: Vec::new(),
-        jxs: Vec::new(),
-        xss: Vec::new(),
+        izaw,
+        nxs,
+        jxs,
+        xss,
     })
+}
+
+fn parse_izaw_array(lines: &mut Lines) -> Result<Vec<(u32, f64)>, AceError> {
+    let mut izaw = Vec::with_capacity(16);
+    for _ in 0..4 {
+        let Some(line) = lines.next() else {
+            return Err(AceError::EndOfFile)
+        };
+        for i in 0..4 {
+            let mut start = i * 18;
+            let mut stop = start + 7;
+            let Ok(iz) = line[start..stop].trim().parse() else {
+                return Err(AceError::Format)
+            };
+            start = stop;
+            stop = start + 11;
+            let Ok(aw) = line[start..stop].trim().parse() else {
+                return Err(AceError::Format)
+            };
+            izaw.push((iz, aw));
+        }
+    }
+    Ok(izaw)
+}
+
+fn parse_nxs_array(lines: &mut Lines) -> Result<Vec<usize>, AceError> {
+    let mut nxs = Vec::with_capacity(16);
+    for _ in 0..2 {
+        let Some(line) = lines.next() else {
+            return Err(AceError::EndOfFile)
+        };
+        for i in 0..8 {
+            let start = i * 9;
+            let stop = i * 9 + 9;
+            let Ok(integer) = line[start..stop].trim().parse() else {
+                return Err(AceError::Format)
+            };
+            nxs.push(integer);
+        }
+    }
+    Ok(nxs)
+}
+
+fn parse_jxs_array(lines: &mut Lines) -> Result<Vec<usize>, AceError> {
+    let mut nxs = Vec::with_capacity(16);
+    for _ in 0..4 {
+        let Some(line) = lines.next() else {
+            return Err(AceError::EndOfFile)
+        };
+        for i in 0..8 {
+            let start = i * 9;
+            let stop = i * 9 + 9;
+            let Ok(integer) = line[start..stop].trim().parse() else {
+                return Err(AceError::Format)
+            };
+            nxs.push(integer);
+        }
+    }
+    Ok(nxs)
+}
+
+fn parse_xss_array(lines: &mut Lines, size: usize) -> Result<Vec<f64>, AceError> {
+    let mut xss = Vec::with_capacity(size);
+    for line in lines {
+        for i in 0..4 {
+            let start = i * 20;
+            let stop = i * 20 + 20;
+            let Ok(float) = line[start..stop].trim().parse() else {
+                return Err(AceError::Format)
+            };
+            xss.push(float);
+        }
+    }
+    Ok(xss)
 }
 
 /// The error type for [`ace`](crate::data::ace) module.
